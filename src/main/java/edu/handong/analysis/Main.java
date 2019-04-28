40b285302d263e9b77b5d6c68a82c451af48973d
JeaminRhee <45053452+JeaminRhee@users.noreply.github.com>
2019-04-28 23:28:54 +0900

    indentation edit

--- src/main/java/edu/handong/analysis/Main.java
+++ src/main/java/edu/handong/analysis/Main.java
@@ -1,7 +1,6 @@
 package edu.handong.analysis;
  
 public class Main {
-
 	public static void main(String[] args) {
 		HGUCoursePatternAnalyzer analyzer = new HGUCoursePatternAnalyzer();
 		analyzer.run(args);
